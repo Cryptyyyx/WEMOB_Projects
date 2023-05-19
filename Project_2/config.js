1a9395b1ee4f42f88a29d0be65037c708a25a406
Arian Kaber <128072972+ArianBTS@users.noreply.github.com>
2023-05-19 22:47:21 +0200

    update

---
+++ Project_2/config.js
@@ -0,0 +1,46 @@
+let colorSize = {
+    "2": {
+        size: 64,
+        color: "#FF5922"
+    },
+    "4": {
+        size: 64,
+        color: "#F35956"
+    },
+    "8": {
+        size: 64,
+        color: "#49BB6C"
+    },
+    "16": {
+        size: 64,
+        color: "#FFBB6C"
+    },
+    "32": {
+        size: 64,
+        color: "#F1C500"
+    },
+    "64": {
+        size: 64,
+        color: "#FFC500"
+    },
+    "128": {
+        size: 36,
+        color: "#2494C1"
+    },
+    "256": {
+        size: 36,
+        color: "#CC94C1"
+    },
+    "512": {
+        size: 36,
+        color: "#9659A7"
+    },
+    "1024": {
+        size: 18,
+        color: "#96BBA7"
+    },
+    "2048": {
+        size: 18,
+        color: "#F4BBAA"
+    }
+  }
